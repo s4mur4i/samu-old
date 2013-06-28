d829b56cac8e798aa87cc54a008520180578b616
Banhidy Krisztian <s4mur4i@balabit.hu>
2013-06-28 08:25:18 +0200

    file_transfer_from_guest.pl: renamed, added new modul sructure, removed unneeded options

--- file_transfer_from_guest.pl
+++ file_transfer_from_guest.pl
@@ -2,83 +2,17 @@
 
 use strict;
 use warnings;
-#use diagnostics;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use lib '/usr/lib/vmware-vcli/apps';
-use Support;
+use SDK::Support;
+use SDK::GuestInternal;
 use VMware::VICommon;
 use VMware::VIRuntime;
 use Data::Dumper;
-#use HTTP::Request;
 use LWP::Simple;
 
-## Fixme need to refactor and put this sub in module
-sub acquireGuestAuth {
-        my ($gOpMgr,$vmview,$gu,$gp) = @_;
-
-        my $authMgr = Vim::get_view(mo_ref => $gOpMgr->authManager);
-        my $guestAuth = NamePasswordAuthentication->new(username => $gu, password => $gp, interactiveSession => 'false');
-
-        eval {
-                print "Validating guest credentials in " . $vmview->name . " ...\n";
-                $authMgr->ValidateCredentialsInGuest(vm => $vmview, auth => $guestAuth);
-        };
-        if($@) {
-                die( "Error: " . $@ . "\n");
-        } else {
-                print "Succesfully validated guest credentials!\n";
-        }
-
-        return $guestAuth;
-}
-
 my %opts = (
-	username => {
-		type => "=s",
-		variable => "VI_USERNAME",
-		help => "Username to ESX",
-		required => 0,
-	},
-	password => {
-		type => "=s",
-		variable => "VI_PASSWORD",
-		help => "Password to ESX",
-		required => 0,
-	},
-	server => {
-		type => "=s",
-		variable => "VI_SERVER",
-		help => "ESX hostname or IP address",
-		default => "vcenter.ittest.balabit",
-		required => 0,
-	},
-	protocol => {
-		type => "=s",
-		variable => "VI_PROTOCOL",
-		help => "http or https, that is the question",
-		default => "https",
-		required => 0,
-	},
-	portnumber => {
-		type => "=i",
-		variable => "VI_PROTOCOL",
-		help => "ESX port for connection",
-		default => "443",
-		required => 0,
-	},
-	url => {
-		type => "=s",
-		variable => "VI_URL",
-		help => "URL for ESX",
-		required => 0,
-	},
-	datacenter => {
-		type => "=s",
-		help => "Datacenter",
-		default => "support",
-		required => 0,
-	},
         vmname => {
                 type => "=s",
                 help => "Name of VM",
@@ -110,7 +44,6 @@ Opts::parse();
 Opts::validate();
 my $username = Opts::get_option('username');
 my $password = Opts::get_option('password');
-my $datacenter = Opts::get_option('datacenter');
 my $vmname = Opts::get_option('vmname');
 my $url = Opts::get_option('url');
 Util::connect( $url, $username, $password );
@@ -136,7 +69,7 @@ if ( (!defined($guestusername)) || (!defined($guestpassword)) || (!defined($vm_v
         die("Cannot run. some paramter failed to be parsed or guessed... or both: username=> '$guestusername' password=> '$guestpassword' vmname=> '" . defined($vm_view) . "'");
 }
 my $guestOpMgr = Vim::get_view(mo_ref => Vim::get_service_content()->guestOperationsManager);
-my $guestCreds = &acquireGuestAuth($guestOpMgr,$vm_view,$guestusername,$guestpassword);
+my $guestCreds = &GuestInternal::acquireGuestAuth($guestOpMgr,$vm_view,$guestusername,$guestpassword);
 my $guestFileMan = Vim::get_view(mo_ref => $guestOpMgr->fileManager);
 
 my $path = Opts::get_option('path');
