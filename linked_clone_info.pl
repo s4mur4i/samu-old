42134c5d6dc4b157f3d7a196ac306f74c37c0c6e
Banhidy Krisztian <s4mur4i@balabit.hu>
2013-06-28 08:21:09 +0200

    linked_clone_info.pl: restructure, removed unneeded options, reindented for better readability

--- linked_clone_info.pl
+++ linked_clone_info.pl
@@ -2,17 +2,13 @@
 ### Find machines linked to the last snapshot of this machine.
 use strict;
 use warnings;
-#use diagnostics;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use lib '/usr/lib/vmware-vcli/apps';
-use Support;
+use SDK::Support;
 use VMware::VICommon;
 use VMware::VIRuntime;
-#use VMware::VILib;
-#use AppUtil::VMUtil;
 use Data::Dumper;
-#use Switch;
 
 sub find_root_snapshot {
 	my ($snapshot) = @_;
@@ -24,63 +20,17 @@ sub find_root_snapshot {
 }
 
 my %opts = (
-username => {
-type => "=s",
-variable => "VI_USERNAME",
-help => "Username to ESX",
-required => 0,
-},
-password => {
-type => "=s",
-variable => "VI_PASSWORD",
-help => "Password to ESX",
-required => 0,
-},
-server => {
-type => "=s",
-variable => "VI_SERVER",
-help => "ESX hostname or IP address",
-default => "vcenter.ittest.balabit",
-required => 0,
-},
-protocol => {
-type => "=s",
-variable => "VI_PROTOCOL",
-help => "http or https, that is the question",
-default => "https",
-required => 0,
-},
-portnumber => {
-type => "=i",
-variable => "VI_PROTOCOL",
-help => "ESX port for connection",
-default => "443",
-required => 0,
-},
-url => {
-type => "=s",
-variable => "VI_URL",
-help => "URL for ESX",
-required => 0,
-},
-datacenter => {
-type => "=s",
-help => "Datacenter",
-default => "support",
-required => 0,
-},
-vmname => {
-type => "=s",
-help => "Get list of linked VM-s to the machine",
-required => 1,
-},
+	vmname => {
+		type => "=s",
+		help => "Get list of linked VM-s to the machine",
+		required => 1,
+	},
 );
 Opts::add_options(%opts);
 Opts::parse();
 Opts::validate();
 my $username = Opts::get_option('username');
 my $password = Opts::get_option('password');
-my $datacenter = Opts::get_option('datacenter');
 my $url = Opts::get_option('url');
 Util::connect( $url, $username, $password );
 
