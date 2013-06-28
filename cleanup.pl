a4df242e4f941d73ac54addcf86e88b782bdbdea
Banhidy Krisztian <s4mur4i@balabit.hu>
2013-06-28 08:29:16 +0200

    cleanup.pl: removed unneeded options

--- cleanup.pl
+++ cleanup.pl
@@ -2,85 +2,31 @@
 
 use strict;
 use warnings;
-#use diagnostics;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use lib '/usr/lib/vmware-vcli/apps';
-use Support;
+use SDK::Support;
 use VMware::VICommon;
 use VMware::VIRuntime;
-#use VMware::VILib;
-#use AppUtil::VMUtil;
 use Data::Dumper;
-#use Switch;
 
 my %opts = (
-	username => {
-		type => "=s",
-		variable => "VI_USERNAME",
-		help => "Username to ESX",
-		required => 0,
-	},
-	password => {
-		type => "=s",
-		variable => "VI_PASSWORD",
-		help => "Password to ESX",
-		required => 0,
-	},
-	server => {
-		type => "=s",
-		variable => "VI_SERVER",
-		help => "ESX hostname or IP address",
-		default => "vcenter.ittest.balabit",
-		required => 0,
-	},
-	protocol => {
-		type => "=s",
-		variable => "VI_PROTOCOL",
-		help => "http or https, that is the question",
-		default => "https",
-		required => 0,
-	},
-	portnumber => {
-		type => "=i",
-		variable => "VI_PROTOCOL",
-		help => "ESX port for connection",
-		default => "443",
-		required => 0,
-	},
-	url => {
-		type => "=s",
-		variable => "VI_URL",
-		help => "URL for ESX",
-		required => 0,
-	},
-	datacenter => {
-		type => "=s",
-		help => "Datacenter",
-		default => "support",
-		required => 0,
-	},
 );
 Opts::add_options(%opts);
 Opts::parse();
 Opts::validate();
 my $username = Opts::get_option('username');
 my $password = Opts::get_option('password');
-my $datacenter = Opts::get_option('datacenter');
 my $url = Opts::get_option('url');
 Util::connect( $url, $username, $password );
 
 my $rp_pools = Vim::find_entity_views( view_type => 'ResourcePool');
-#my $parent_rp = Vim::find_entity_view( view_type => 'ResourcePool', filter => { name => 's4mur4i' });
 
 foreach my $pool (@$rp_pools) {
 	if ( ! defined($pool->resourcePool)) {
-		#print "No Child resource pools in Resource pool: " . $pool->name . "\n";
 		if ( ! defined($pool->vm) ) {
-		#	print "There are no child vm-s: " . $pool->name . "\n";
 			my $vim=Vim::get_vim;
 			my $path = Util::get_inventory_path( $pool, $vim );
-			#print "$path\n";
 			if ( $path =~ m/^\s*Support\/host\/[^\/]*\/Resources\/[^\/]*\s*$/)  {
 				print "Top level Resource pool.. just empty: " . $pool->name . "\n";
 			} else {
