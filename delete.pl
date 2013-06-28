f1fe35614eaaf198e126324f80976d9f34aed53d
Banhidy Krisztian <s4mur4i@balabit.hu>
2013-06-28 08:26:42 +0200

    delete.pl: removed unneded options

--- delete.pl
+++ delete.pl
@@ -2,17 +2,12 @@
 
 use strict;
 use warnings;
-#use diagnostics;
 use FindBin;
 use lib "$FindBin::Bin/lib";
-use lib '/usr/lib/vmware-vcli/apps';
-use Support;
+use SDK::Support;
 use VMware::VICommon;
 use VMware::VIRuntime;
-#use VMware::VILib;
-#use AppUtil::VMUtil;
 use Data::Dumper;
-#use Switch;
 
 sub single($) {
         my ($vmname) = @_;
@@ -58,51 +53,6 @@ sub ticket($) {
 };
 
 my %opts = (
-	username => {
-		type => "=s",
-		variable => "VI_USERNAME",
-		help => "Username to ESX",
-		required => 0,
-	},
-	password => {
-		type => "=s",
-		variable => "VI_PASSWORD",
-		help => "Password to ESX",
-		required => 0,
-	},
-	server => {
-		type => "=s",
-		variable => "VI_SERVER",
-		help => "ESX hostname or IP address",
-		default => "vcenter.ittest.balabit",
-		required => 0,
-	},
-	protocol => {
-		type => "=s",
-		variable => "VI_PROTOCOL",
-		help => "http or https, that is the question",
-		default => "https",
-		required => 0,
-	},
-	portnumber => {
-		type => "=i",
-		variable => "VI_PROTOCOL",
-		help => "ESX port for connection",
-		default => "443",
-		required => 0,
-	},
-	url => {
-		type => "=s",
-		variable => "VI_URL",
-		help => "URL for ESX",
-		required => 0,
-	},
-	datacenter => {
-		type => "=s",
-		help => "Datacenter",
-		default => "support",
-		required => 0,
-	},
 	ticket => {
 		type => "=s",
 		help => "The ticket resource pool",
@@ -119,7 +69,6 @@ Opts::parse();
 Opts::validate();
 my $username = Opts::get_option('username');
 my $password = Opts::get_option('password');
-my $datacenter = Opts::get_option('datacenter');
 my $url = Opts::get_option('url');
 Util::connect( $url, $username, $password );
 my $ticket = Opts::get_option('ticket');
