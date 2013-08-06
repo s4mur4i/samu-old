31d85f76b60b23303c998086c6cdd55a4c84f422
Banhidy Krisztian <s4mur4i@balabit.hu>
2013-08-06 14:07:29 +0200

    ticket_list.pl: added support for multiple kayako tickets

--- ticket_list.pl
+++ ticket_list.pl
@@ -8,6 +8,7 @@ use lib '/usr/lib/vmware-vcli/apps';
 use SDK::Misc;
 use LWP::Simple qw(get);
 use SDK::Kayako;
+use SDK::Bugzilla;
 use VMware::VICommon;
 use VMware::VIRuntime;
 my %opts = (
@@ -37,30 +38,33 @@ for my $ticket ( sort (keys %tickets) ) {
 		} else {
 			print ", ticket status: " . $$result{ticketstatustitle} ."";
 			$result = &Kayako::run_query( $dbh, "select fieldvalue from swcustomfieldvalues where typeid = '$ticket' and customfieldid = '25'" );
-		#print Dumper($result);
-			if ( !defined( $result ) or $$result{fieldvalue} eq "" ) {
+			if ( !defined($result) or $$result{fieldvalue} eq "" ) {
 				print "\n";
 			} else {
-				my $id;
-				if ($$result{fieldvalue} =~ /^\s*\d+\s*$/ ) {
-					$id = $$result{fieldvalue};
-				} elsif ($$result{fieldvalue} =~ /\?id=\d+/ ) {
-					( $id ) = $$result{fieldvalue} =~ /id=(\d+)\D?/ ;
-				} else {
-					$id = $$result{fieldvalue};
-				}
-				print ", bugzilla: " . $id;
-				my $url = "http://bugzilla.balabit/bugzilla-3.0/show_bug.cgi?id=$id";
-				my $content;
-				for my $line ( split qr/\R/, get( $url ) ) {
-					( $content ) = $line =~ /\s*<span id="static_bug_status">(\w+)/ if ( $line =~ /<span id="static_bug_status">/ ) ;
-				}
-				if ( !defined( $content ) ) {
-					print "\n";
-				} else {
-					print ", bugzilla status: $content\n";
-				}
+				my @result = split( " ", $$result{fieldvalue} );
+				foreach ( @result ) {
+					#print " result: '$_'\n";
+					if ( $_ eq "" ) {
+						print "\n";
+					} else {
+						my $id;
+						if ($_ =~ /^\s*\d+\s*$/ ) {
+							$id = $_;
+						} elsif ($_ =~ /\?id=\d+/ ) {
+							( $id ) = $_ =~ /id=(\d+)\D?/ ;
+						} else {
+							$id = $_;
+						}
+						print ", bugzilla: " . $id;
+						my $content = &Bugzilla::bugzilla_status( $id );
+						if ( !defined( $content ) ) {
+							print "\n";
+						} else {
+							print ", bugzilla status: $content\n";
+						}
 
+					}
+				}
 			}
 		}
 	}
