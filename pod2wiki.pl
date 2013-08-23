73971dee3085db05e3a9d45634c2feef1c6a741e
Banhidy Krisztian <s4mur4i@balabit.hu>
2013-08-23 15:12:14 +0200

    pod2wiki: initial commit

---
+++ pod2wiki.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/lib";
+use Pod::Simple::Wiki::Dokuwiki;
+
+
+my $parser = Pod::Simple::Wiki->new('dokuwiki');
+
+if ( defined $ARGV[0] ) {
+    open IN, $ARGV[0] or die "Couldn't open $ARGV[0]: $!\n";
+}
+else {
+    *IN = *STDIN;
+}
+
+if ( defined $ARGV[1] ) {
+    open OUT, ">$ARGV[1]" or die "Couldn't open $ARGV[1]: $!\n";
+}
+else {
+    *OUT = *STDOUT;
+}
+
+$parser->output_fh( *OUT );
+$parser->parse_file( *IN );
+
+__END__
