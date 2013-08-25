5ba80eecca5debab49dc89e7a41a177aa3d12375
Banhidy Krisztian <s4mur4i@balabit.hu>
2013-08-25 11:28:59 +0200

    TEST_PERL_MODULES.pl: added summary at end of print

--- TEST_PERL_MODULES.pl
+++ TEST_PERL_MODULES.pl
@@ -4,11 +4,14 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+my @error;
+my $count=0;
 sub test {
 	my ( $var, $name) = @_;
 	if ( $var ) {
 		print "$name error\n";
 		print Dumper( $var );
+        push( @error, $name );
 	} else {
 		print "$name loaded succesfully\n";
 	}
@@ -16,6 +19,7 @@ sub test {
 
 sub req {
 	my ( $name ) = @_;
+    $count++;
 	eval "require $name";
 	&test( $@, $name );
 }
@@ -31,3 +35,12 @@ foreach ( @modules) {
 	print "Testing module: $_\n";
 	&req($_);
 }
+print "#### Summary BEGIN ####\n";
+print "Tested $count perl modules\n";
+if ( @error ne 0 ) {
+    print "There were " . @error . " errors\n";
+    print "Need to fix problems with: " . join( ", ", @error ) . "\n";
+} else {
+    print "Everything ok.\n";
+}
+print "#### Summary END ####\n";
