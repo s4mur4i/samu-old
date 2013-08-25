f516aafcd9bb952d8d7542873574033671851657
Banhidy Krisztian <s4mur4i@balabit.hu>
2013-08-25 11:21:48 +0200

    generate_perl_modules.sh: removed removal of perl version, now it is required

--- generate_perl_modules.sh
+++ generate_perl_modules.sh
@@ -3,6 +3,6 @@ file="PERL_MODULES"
 ## generate normal modules
 egrep -Rh ^use\s*[^:]*::[^:]* * | perl -ne 'chomp $_ ;my ( $var ) = $_ =~ /^use\s*([^ :]*::[^: ;]*)[ ;]?/;if ($var ne "" ) { print "$var\n" }' |egrep -v 'VMware|AppUtil|SDK|BB|Base' |sort -u > $file
 ## generate single module
-egrep -Rh "^use\s*[^: ]*[\s;]+$"| sort -u|egrep -v "strict|warnings|constant" | perl -ne 'chomp $_ ; my ($var) = $_ =~ /^use\s*([^ ;]*)/; if ( $var ne "" ) {print "$var\n"}' |grep -v 5.006001 >> $file
+egrep -Rh "^use\s*[^: ]*[\s;]+$"| sort -u|egrep -v "strict|warnings|constant" | perl -ne 'chomp $_ ; my ($var) = $_ =~ /^use\s*([^ ;]*)/; if ( $var ne "" ) {print "$var\n"}' >> $file
 ## static module
 echo "DBD::mysql" >> $file
