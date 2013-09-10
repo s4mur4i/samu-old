f69cbcbd460cbed7050129071a349c2b07c513d7
Banhidy Krisztian <s4mur4i@balabit.hu>
2013-09-10 15:01:11 +0200

    generate_perl_modules: updated detect

--- generate_perl_modules.sh
+++ generate_perl_modules.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 file="PERL_MODULES"
 ## generate normal modules
-egrep -Rh ^use\s*[^:]*::[^:]* * | perl -ne 'chomp $_ ;my ( $var ) = $_ =~ /^use\s*([^ :]*::[^: ;]*)[ ;]?/;if ($var ne "" ) { print "$var\n" }' |egrep -v 'VMware|AppUtil|SDK|BB|Base' |sort -u > $file
+egrep -Rh "^use\s*[^:]*::[^: ]*|require\s*[^:]*::[^: ]*" * | perl -ne 'chomp $_ ;my ( $var ) = $_ =~ /([^ :]*::[^: ;]*)/;if ($var ne "" ) { print "$var\n" }' |egrep -v 'VMware|AppUtil|SDK|BB|Base|FindBin|Vim|Win32|GSSAPI' |sort -u > $file
 ## generate single module
-egrep -Rh "^use\s*[^: ]*[\s;]+$"| sort -u|egrep -v "strict|warnings|constant|5." | perl -ne 'chomp $_ ; my ($var) = $_ =~ /^use\s*([^ ;]*)/; if ( $var ne "" ) {print "$var\n"}' >> $file
+egrep -Rh "^use\s*[^: ]*[\s;]+$" * | sort -u|egrep -v "strict|warnings|constant|5." | perl -ne 'chomp $_ ; my ($var) = $_ =~ /^use\s*([^ ;]*)/; if ( $var ne "" ) {print "$var\n"}' >> $file
 ## static module
 echo "DBD::mysql" >> $file
