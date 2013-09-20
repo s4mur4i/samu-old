0d0db5043ef8626d7f700bc698a4fecb73852450
Banhidy Krisztian <s4mur4i@balabit.hu>
2013-09-20 16:19:49 +0200

    generate_perl_modules.sh: fixed Test::Perl::Critic detection

--- generate_perl_modules.sh
+++ generate_perl_modules.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 file="PERL_MODULES"
 ## generate normal modules
-egrep -Rh "^use\s*[^:]*::[^: ]*|require\s*[^:]*::[^: ]*" * | perl -ne 'chomp $_ ;my ( $var ) = $_ =~ /([^ :]*::[^: ;]*)/;if ($var ne "" ) { print "$var\n" }' |egrep -v 'VMware|AppUtil|SDK|BB|Base|FindBin|Vim|Win32|GSSAPI' |sort -u > $file
+egrep -Rh "^use\s*[^:]*::[^: ]*|require\s*[^:]*::[^: ]*" * | perl -ne 'chomp $_ ;my ( $var ) = $_ =~ /([^ :]*::[^: ;]*)/;if ($var ne "" ) { print "$var\n" }' |egrep -v 'VMware|AppUtil|SDK|BB|Base|FindBin|Vim|Win32|GSSAPI|Test::Perl' |sort -u > $file
 ## generate single module
 egrep -Rh "^use\s*[^: ]*[\s;]+$" * | sort -u|egrep -v "strict|warnings|constant|5." | perl -ne 'chomp $_ ; my ($var) = $_ =~ /^use\s*([^ ;]*)/; if ( $var ne "" ) {print "$var\n"}' >> $file
 ## static module
 echo "DBD::mysql" >> $file
+echo "Test::Perl::Critic" >> $file
