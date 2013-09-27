593dfd23622f865baa2656438e86d7cac2161aef
Krisztian Banhidy <s4mur4i@balabit.hu>
2013-09-27 21:46:49 +0200

    network.pm: create list_switch and list_dvp sub

--- lib/Base/network.pm
+++ lib/Base/network.pm
@@ -19,15 +19,33 @@ our $module_opts = {
     functions => {
         add => {
             function => \&network_add,
-            opts => {
+            opts     => {
                 ticket => {
-                    entity => "=s",
-                    help => "Ticket to add network to",
+                    type     => "=s",
+                    help     => "Ticket to add network to",
                     required => 1,
                 },
                 type => {
-                    entity => "=s",
-                    help => "Type of network to add",
+                    type     => "=s",
+                    help     => "Type of network to add",
+                    required => 1,
+                },
+            },
+        },
+        list_switch => {
+            function => \&list_switch,
+            opts     => {},
+        },
+        list_dvp => {
+            function => \&list_dvp,
+            opts     => {},
+        },
+        delete => {
+            function => \&network_delete,
+            opts     => {
+                name => {
+                    type     => "=s",
+                    help     => "Name of switch to delete",
                     required => 1,
                 },
             },
@@ -42,6 +60,52 @@ sub main {
 
 sub network_add {
     &Log::debug("Starting network::network_add sub");
+    my $ticket = Opts::get_option('ticket');
+    my $type   = Opts::get_option('type');
+    &Log::debug("Requested options, ticket=>'$ticket', type=>'$type'");
+    my $name = $ticket . "-" . $type . "-" . &Misc::random_3digit;
+    while ( &VCenter::exists_entity( $name, 'DistributedVirtualPortgroup' ) ) {
+        $name = $ticket . "-" . $type . "-" . &Misc::random_3digit;
+    }
+    if ( !&VCenter::exists_entity( $ticket, 'DistributedVirtualSwitch' ) ) {
+        &Log::debug("Switch does not exist, need to create");
+        &VCenter::create_switch($ticket);
+    }
+    else {
+        &Log::debug("Switch already exists");
+    }
+    &VCenter::create_dvportgroup( $name, $ticket );
+    &Log::debug("Finished creating port group");
+    return 1;
+}
+
+sub list_switch {
+    &Log::debug("Starting network::list_switch sub");
+    my $views = Vim::find_entity_views( view_type => 'Network', properties => [ 'name' ] );
+    if ( !defined( $viewss ) ) {
+        Entity::NumException->throw( error => 'No switch found', entity => 'Network', count => '0' );
+    }
+    foreach( @$views ) {
+        &Log::dumpobj( "switch", $_ );
+        print "name:" . $_->name . "\n";
+    }
+}
+
+sub list_dvp {
+    &Log::debug("Starting network::list_dvp sub");
+    my $networks = Vim::find_entity_views( view_type => 'DistributedVirtualPortgroup', properties => [ 'name' ] );
+    if ( !defined( $networks ) ) {
+        Entity::NumException->throw( error => 'No DVP found', entity => 'DistributedVirtualPortGroup', count => '0' );
+    }
+    foreach( @$networks ) {
+        &Log::dumpobj( "dvp", $_ );
+        print "name:" . $_->name . "\n";
+    }
+
+}
+
+sub network_delete {
+    &Log::debug("Starting network::network_delete sub");
 }
 
 1;
